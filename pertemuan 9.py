ec7b2e594bc2099b56d756a1297b4490787a44ec
Rania Yasmin <raniayasmin01@gmail.com>
2023-11-04 15:43:19 +0700

    Menambahkan file streamlit

---
+++ pertemuan 9.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+st.title("Kuliah Analisis Big Data") 
+st.write("Rania Yasmin")
+st.write("# Heading 1")
+st.write ("## Heading 2")
+st.write("### Heading 3")
+
+pilih1 = st.checkbox('Ya')
+pilih2 = st.checkbox('Tidak')
+
+st.write (pilih1)
+st.write (pilih2)
+
+makanan= st.radio('Makanan Kesukaan', ['Bakso','Nasi Goreng','Mie Ayam'])
+st.write (makanan)
+
+minuman = st.selectbox('Pilih minuman yang akan dipesan', 
+            ['Es Teh', 'Kopi', 'Jus'])
+
+st.write (minuman)
+
+
+bayar = st.multiselect('Bayar Pakai:',
+                       ['Tunai', 'Ovo', 'Gopay'])
+st.write(bayar)
